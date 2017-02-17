001b8dade49cd5f15f29d23b085df1011cc93675
John Richards <john@pushpoplabs.com>
2017-02-16 23:44:48 -0500

    Sets global surface format to use OpenGL 4.0

--- QtSurfaces/main.cpp
+++ QtSurfaces/main.cpp
@@ -10,6 +10,7 @@
 
 #include <QApplication>
 #include <QString>
+#include <QSurfaceFormat>
 
 #include "mainwindow.h"
 #include "surface.h"
@@ -20,7 +21,14 @@ int main(int argc, char *argv[])
     std::map<QString, Surface*> surfaces;
     surfaces[QString("Torus")] = new Torus();
 
+    // Set global format for using OpenGL 4.0.
+    QSurfaceFormat format;
+    format.setVersion(4, 0);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(format);
+
     QApplication a(argc, argv);
+
     MainWindow w(surfaces);
     w.show();
 
